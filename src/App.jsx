a485cc8a77a636248c9f9dfcf7f79bc2408699fa
karinareyes25 <karinalrg16@gmail.com>
2025-01-23 07:23:28 +0100

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import { SketchPicker } from "react-color";
+import Title from "./components/Title/title";
+import Selected from "./components/Selected/selected";
+
 
 function App() {
-  const [currentColor, setCurrentColor] = useState("#ff6");
-  const handleOnChange = (color) => {
-    setCurrentColor(color.hex);
-  };
-
-  const selectedStyle = {
-    color: "black",
-    backgroundColor: currentColor,
-    height: "90px",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "solid 3px gray",
-    width: "215px",
-    borderRadius: "25px",
-  };
-
-  const textStyle = {
-    alignItems: "center",
-    justifyContent: "center",
-    width: "200px",
-    height: "100px",
-    padding: "15px",
-  };
-
-  const titleStyle = {
-    alignItems: "center",
-    justifyContent: "center",
-    width: "200px",
-    height: "100px",
-  };
-
   return (
-    <div>
-      <h1 className="Title" style={titleStyle}>
-        Color Picker
-      </h1>
-      <div className="Selected" style={selectedStyle}>
-        <p className="Text" style={textStyle}>
-          Selected Color:{currentColor}{" "}
-        </p>
-      </div>
 
-      <SketchPicker color={currentColor} onChangeComplete={handleOnChange} />
-    </div>
+    <>
+      <Title />
+      <Selected/>
+    
+    </>
   );
+
 }
 
 export default App;
---
+++ src/components/Title/title.jsx
@@ -0,0 +1,10 @@
+import "./title.css";
+export default function Title (){
+
+    return(
+
+        <div>
+            <h1 className="title">Color Picker</h1>
+        </div>
+    )
+} 
